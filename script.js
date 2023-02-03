cf0855deee0f0ead279c4bd90c6a241b1da5a803
Agia <agia@pm.me>
2023-02-03 13:32:55 +0000

    Added a heading to translations area, styled search button a touch, commented out unused JS and removed dotfile from repo

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-let stuff;
-fetch("https://countryapi.io/api/name/austria?apikey=JDi0TGPbrqRCyHdRBVDEJVoP73cKyeehMnzV8PET")
-    .then((response) => response.json())
-    .then((data) => {
-        stuff = data;
-        console.log(data)
-    });
+// Commented this out as it makes an API call everytime one of us changes anything
+
+// let stuff;
+// fetch("https://countryapi.io/api/name/austria?apikey=JDi0TGPbrqRCyHdRBVDEJVoP73cKyeehMnzV8PET")
+//     .then((response) => response.json())
+//     .then((data) => {
+//         stuff = data;
+//         console.log(data)
+//     });
 
